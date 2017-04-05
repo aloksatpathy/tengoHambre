2ba0f265330fe7ca3d573f01570c1d8a0f47911a
Alok Satpathy <aloks1990@gmail.com>
2017-04-05 12:18:34 -0500

    added changes for deployment

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 S3_LOCATION = 'http://tengohambre.s3.amazonaws.com/'
-S3_KEY = 'AKIAINRHXLAIV263BDDA'
-S3_SECRET = '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'
+S3_KEY = 'AKIAIUD3WMGAM2L22HNQ'
+S3_SECRET = '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'
 S3_UPLOAD_DIRECTORY = 'tengohambre'
 S3_BUCKET = 'tengohambre'
 
